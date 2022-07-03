6d82662095deadc41ef2fc9428196adbc0f24d7c
«Anna» <atyna08@mail.ru>
2022-07-03 23:38:49 +0300

    Домашнее задание

---
+++ Ex003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Zadacha17(number);
+
+void Zadacha17(int number)
+{
+    Console.WriteLine();
+    int count = 1;
+    while (number >= count)
+    {
+        Console.WriteLine($"{Math.Pow((count),3)}");
+        count++;
+    }
+}
